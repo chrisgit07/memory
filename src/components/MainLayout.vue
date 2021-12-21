dc37ce4953db4aa355bb49b40f816b22333df958
christopher <christopher.bonhomme@gmail.com>
2021-12-21 10:06:27 +0100

    layout

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <Header></Header>
+    <Page></Page>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Page from './Page.vue'
+import Footer from './Footer.vue'
+
+export default {
+  name: 'mainLayout',
+  components:{
+    Header,
+    Page,
+    Footer
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
